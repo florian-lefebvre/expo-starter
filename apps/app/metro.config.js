d2fbabca92f95f8d0ee1be13745c9b6fc442ce3c
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-07-09 15:57:34 +0200

    nativewind

--- apps/app/metro.config.js
+++
@@ -1,7 +0,0 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-import { getDefaultConfig } from 'expo/metro-config';
-
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-
-export default config;
